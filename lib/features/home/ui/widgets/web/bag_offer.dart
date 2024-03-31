99936c0a89c8de68fdcddec744dec02556f246e9
abdalrahmanreda24 <abdulrahman.20377239@compit.aun.edu.eg>
2024-03-31 23:57:24 +0200

    - add bag offer

--- lib/features/home/ui/widgets/web/bag_offer.dart
+++ lib/features/home/ui/widgets/web/bag_offer.dart
@@ -1,6 +1,8 @@
 import 'package:e_commerce_web/core/constant/app_constant.dart';
+import 'package:e_commerce_web/features/home/ui/widgets/web/image_offering.dart';
+import 'package:e_commerce_web/features/home/ui/widgets/web/timer_part.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../../config/colors/app_colors.dart';
@@ -11,19 +13,18 @@ class HomeBagOffer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 500.h,
+      padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
+      height: 600.h,
       width: AppConstant.deviceWidth(context),
       color: AppColors.kTanColoe,
-      child: Row(
+      child: const Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          CircleAvatar(
-            radius: 50.r,
-            backgroundColor: AppColors.kBlackColor,
-            child: Text(
-              AppLocalizations.of(context)!.saleOff,
-            ),
+          Expanded(
+            flex: 2,
+            child: ImageOffering(),
           ),
+          TimerPart(),
         ],
       ),
     );
